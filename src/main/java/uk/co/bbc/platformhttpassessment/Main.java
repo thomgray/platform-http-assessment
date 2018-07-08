6d75d0209888a62c697c1550fbbe78939397ba65
Thom Gray <thom.gray@bbc.co.uk>
2018-07-08 20:48:26 +0100

    tidying up

--- src/main/java/uk/co/bbc/platformhttpassessment/Main.java
+++ src/main/java/uk/co/bbc/platformhttpassessment/Main.java
@@ -11,6 +11,9 @@ interface MainComponent {
 
 public class Main {
     public static void main(String[] args) {
+        // silence apache logging
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
         MainComponent mainComponent = DaggerMainComponent.create();
         mainComponent.getApplication().run(args);
     }
--- src/main/java/uk/co/bbc/platformhttpassessment/modules/MainModule.java
+++ src/main/java/uk/co/bbc/platformhttpassessment/modules/MainModule.java
@@ -5,14 +5,13 @@ import dagger.Provides;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.params.HttpParams;
 
 
 @Module
 public class MainModule {
     private static final int MS_PER_SEC = 1000;
     private static final int TIMEOUT_SECS = 5;
+
     @Provides
     HttpClient provideHttpClient() {
         RequestConfig config = RequestConfig.custom()
