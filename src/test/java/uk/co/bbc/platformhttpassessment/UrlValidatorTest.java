470aeb00e486a508d7bb0dcb3e013e8685e01616
Thom Gray <thom.gray@bbc.co.uk>
2018-07-08 14:38:57 +0100

    adding tests for url validation

--- src/test/java/uk/co/bbc/platformhttpassessment/UrlValidatorTest.java
+++ src/test/java/uk/co/bbc/platformhttpassessment/UrlValidatorTest.java
@@ -3,15 +3,34 @@ package uk.co.bbc.platformhttpassessment;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.net.URI;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UrlValidatorTest {
-    private static final String URL_WITH_INVALID_PROTOCOL = "bad://www.foo.com";
-    private static final String URL_WITH_SPACE = "https://www.foo and bar.com";
+    final UrlValidator underTest = new UrlValidator();
 
     @ParameterizedTest
-    @ValueSource(strings = {URL_WITH_INVALID_PROTOCOL, URL_WITH_SPACE})
-    void returnsEmptyOptionIfUrlIsInvalid() {
+    @ValueSource(strings = {
+            "http://www.foo.com",
+            "https://foo.bar?with=param"
+    })
+    void shouldReturnOptionalOfUrlForValidUrls(String url) {
+        Optional<URI> result = underTest.getValidUri(url);
 
+        assertEquals(Optional.of(URI.create(url)), result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "bad://www.foo.com",
+            "https://www.foo with spaces"
+    })
+    void returnsEmptyOptionIfUrlIsInvalid(String url) {
+        Optional<URI> result = underTest.getValidUri(url);
+
+        assertEquals(Optional.empty(), result);
     }
 
 }
