386c08c60d4733c043173af41afed22323bd3b50
Thom Gray <thom.gray@bbc.co.uk>
2018-07-08 15:15:28 +0100

    written unit test for output' gst

--- src/main/java/uk/co/bbc/platformhttpassessment/Application.java
+++ src/main/java/uk/co/bbc/platformhttpassessment/Application.java
@@ -1,13 +1,11 @@
 package uk.co.bbc.platformhttpassessment;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Streams;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import uk.co.bbc.platformhttpassessment.domain.HttpGetResult;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -21,19 +19,23 @@ public class Application {
 
     private final UrlGetter urlGetter;
     private final Executor executor;
+    private final ObjectMapper objectMapper;
 
     @Inject
     public Application(UrlGetter urlGetter) {
         this.urlGetter = urlGetter;
         executor = Executors.newFixedThreadPool(5);
+        objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
     public void run(String[] args) {
         CompletionService<HttpGetResult> resultCompletionService = new ExecutorCompletionService<>(executor);
-        Stream<Future<HttpGetResult>> results = Stream.of(args)
-                .map((url) -> resultCompletionService.submit(() -> urlGetter.get(url)));
 
-        List<HttpGetResult> res = results.map(this::getResultFromFuture).collect(Collectors.toList());
+        List<HttpGetResult> results = Stream.of(args)
+                .map((url) -> resultCompletionService.submit(() -> urlGetter.get(url)))
+                .map(this::getResultFromFuture).collect(Collectors.toList());
+
     }
 
     private HttpGetResult getResultFromFuture(Future<HttpGetResult> future) {
--- src/test/java/uk/co/bbc/platformhttpassessment/ApplicationTest.java
+++ src/test/java/uk/co/bbc/platformhttpassessment/ApplicationTest.java
@@ -1,13 +1,23 @@
 package uk.co.bbc.platformhttpassessment;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.co.bbc.platformhttpassessment.domain.HttpGetResult;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicationTest {
@@ -17,6 +27,9 @@ class ApplicationTest {
 
     @Mock
     UrlGetter urlGetter;
+
+    @Rule
+    SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
     @BeforeEach
     void setup() {
@@ -32,6 +45,28 @@ class ApplicationTest {
         verify(urlGetter).get(URL_1);
         verify(urlGetter).get(URL_2);
         verifyNoMoreInteractions(urlGetter);
+    }
+
+    @Test
+    void shouldLogResulsAsJsonStream() throws Exception {
+        HttpGetResult result1 = new HttpGetResult(URL_1, 200, 100L, "Date now", null);
+        HttpGetResult result2 = new HttpGetResult(URL_2, 200, 200L, "Date now", null);
+
+        when(urlGetter.get(eq(URL_1))).thenAnswer((invocationOnMock -> {
+            if (invocationOnMock.getArgument(0).equals(URL_1)) {
+                return result1;
+            } else return result2;
+        }));
+
+        underTest.run(new String[]{URL_1, URL_2});
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        String expectedOutput = objectMapper.writeValueAsString(List.of(result1, result2));
+
+        System.out.println(expectedOutput);
+        assertEquals(expectedOutput, systemOutRule.getLog());
     }
 
 
